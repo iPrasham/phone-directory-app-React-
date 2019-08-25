250c9d504760cc9c541806be62cd038474bf78f9
Prasham Jain <prshmjain1@gmail.com>
2019-08-25 15:20:51 +0530

    Added Suscribers

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let suscribers = [
+      {
+        id: 1,
+        name: "Prasham",
+        phone: "9529528212"
+      },
+      {
+        id: 2,
+        name: "Sakshi",
+        phone: "7011372429"
+      }
+    ];
     return (
       <div>
         <div>
@@ -17,7 +29,21 @@ class App extends Component {
           <span className="spanName">Name</span>
           <br />
           <span className="spanPhone">Phone</span>
+          <br/>
+          <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
         </div>
+
+        {
+          suscribers.map(sub => {
+            return <div key={sub.id} className="flexBox">
+                <span className="name">{sub.name}</span>
+                <br />
+                <span className="phone">{sub.phone}</span>
+                <br/>
+                <button className="deleteButton">DELETE</button>
+              </div>
+          })
+        }
       </div>
     );
   }
