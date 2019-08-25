87922d14c9840f54e6eedda4ec21f96c67abd338
Prasham Jain <prshmjain1@gmail.com>
2019-08-25 13:59:54 +0530

    Created Header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from "./Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import logo from './logo.svg';
+// import './App.css';
 
-export default App;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <Header />
+          <button>ADD</button>
+        </div>
+        <div>
+          <span>Name</span>
+          <br />
+          <span>Phone</span>
+        </div>
+      </div>
+    );
+  }
+ }
+    
+    export default App;
